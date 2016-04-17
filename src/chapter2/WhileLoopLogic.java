051c34e03b4f1b48f1de718e83ca076bd7c98382
dilipkumar <dilipkumar@192.168.99.1>
2016-04-16 23:07:53 -0700

    Add my files

---
+++ src/chapter2/WhileLoopLogic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter2;
+
+/**
+ *
+ * @author dilipkumar
+ */
+public class WhileLoopLogic {
+   
+     public static void main (String [] argv) {
+        
+        System.out.println("Hello WhileLoopLogic !!!");
+        
+        int i=10;
+        
+        while (i >0 ) {
+          System.out.println(" Counter at "+ i);   
+        i--;
+        }
+        
+    }
+}
